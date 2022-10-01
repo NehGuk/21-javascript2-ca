a06b5ff3b6ff523dc2f8719d5200aa1fc3b81eef
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-01 14:38:49 +0200

    Signup form and API call ready

---
+++ scripts/api/api-urls.mjs
@@ -0,0 +1,17 @@
+// BASE URL
+export const API_BASE_URL = `https://nf-api.onrender.com`;
+
+// END POINTS
+// register
+export const registerURL = `${API_BASE_URL}/api/v1/social/auth/register`;
+
+// login
+
+// etc
+
+
+// initial register function trial
+
+    // Once new user is created, do an API call to register the new user
+
+   
