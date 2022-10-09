7299a7d4c1aa230d3a3ee5b32aa704c874e82ffa
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-09 13:54:32 +0200

    Form data and function setup

--- scripts/api/api-call-create-new-entry.mjs
+++ scripts/api/api-call-create-new-entry.mjs
@@ -1,15 +1,5 @@
-export async function createNewEntry(url, entryContent) {
-    const token = localStorage.getItem("accessToken");
-    const postData = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(entryContent),
-    }
-    const response = await fetch(url, postData);
-    const postEntry = await (response).json(); 
+export async function sendNewPost() {
+    console.log("Sending new post");
+
 };
 
-// updating file
--- scripts/index-create-new-entry.mjs
+++ scripts/index-create-new-entry.mjs
@@ -1,32 +1,25 @@
-// API URLs
-import { newEntryURL } from "./api/api-urls.mjs";
+import { sendNewPost } from "./api/api-call-create-new-entry.mjs";
 
-// Function to create new entry
-import { createNewEntry } from "./api/api-call-create-new-entry.mjs";
 
 // Form variables
 const form = document.querySelector("#add-post-form");
+console.log(form);
 
-// Add event listener to PUBLISH button
-form.addEventListener("submit", createPostAndSendEntry);
+// Form event listener
+form.addEventListener("submit", createAndSendPost);
 
-function createPostAndSendEntry(event) {
+async function createAndSendPost(event) {
     event.preventDefault();
-    console.log("Clicked!");
-    
-    // Create object from form fields
+    event.target.reset();
+    console.log("Working");
+
+    // Create post object from form fields
     const myFormData = new FormData(event.target);
     const postContent = Object.fromEntries(myFormData.entries());
     console.log(postContent);
 
-    // Call function to create new entry
-    createNewEntry(newEntryURL, postContent);
-    
-    // Refresh the post feed somehow
-    location.href = form.action;
-
-    // another option is event.target.reset();, and then refresh the feed by calling the function again.
+    // Send post object to API
+    sendNewPost();
 
 };
 
-// updating file
