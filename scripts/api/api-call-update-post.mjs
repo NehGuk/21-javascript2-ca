e026249e73757731acc90ff224f68044e6a26c9c
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-09 18:23:00 +0200

    Files, functions and variables initialised

--- scripts/api/api-call-update-post.mjs
+++ scripts/api/api-call-update-post.mjs
@@ -1,1 +1,11 @@
-// Update post
+export async function updateEntry() {
+    try {
+        console.log("Updating entry");
+
+        const postContainer = 
+
+
+    } catch(error) {
+        console.log(error);
+    }
+};
