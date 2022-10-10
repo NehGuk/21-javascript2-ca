53fb0023116f13804aca6cad95360a506a6b9c0e
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-10 10:49:58 +0200

    User profile info ready

--- scripts/api/api-call-get-user-profile.mjs
+++ scripts/api/api-call-get-user-profile.mjs
@@ -17,17 +17,33 @@ export async function getUserProfile(url) {
         const userProfile = await response.json();
         console.log(response);
         console.log(userProfile);
+        console.log(userProfile.name);
+        console.log(userProfile.email);
 
         async function displayUserProfile() {
-            console.log("Displaying");
+
+            if (!userProfile.avatar) {
+                userProfile.avatar = "/assets/image-sample.jpg";
+            }
+
+            const profileContainer = document.querySelector("#profile-main-container");
+            profileContainer.innerHTML = "";
+            profileContainer.innerHTML = `
+
+                <h1 class="text-center">My profile</h1>
+                <div class="text-center">
+                <img src="${userProfile.avatar}" alt="Bootstrap" width="200" height="200" class="rounded-circle border border-white mb-4">
+                </div>
+                <p class="text-center"><b>Name: </b>${userProfile.name}</p>
+                <p class="text-center"><b>Email: </b>${userProfile.email}</p>
+                <hr>
+            
+            `;
         }
         displayUserProfile();
-
-
     } catch(error) {
         console.log(error);
     }
-
 }
 
 //getUserProfile();
--- scripts/profile.mjs
+++ scripts/profile.mjs
@@ -1,15 +1,9 @@
-// After login, direct user to her respective profile page
-
-/// API URL 
+// API URL 
 import { userProfileURL } from "./api/api-urls.mjs";
 import { extraProfileURL } from "./api/api-urls.mjs";
 const userName = localStorage.getItem("userName");
-
 const userURLToLoad = `${userProfileURL}${userName}${extraProfileURL}`;
 console.log(userURLToLoad);
-
-
-
 
 // Function to load user profile info
 import { getUserProfile } from "./api/api-call-get-user-profile.mjs";
