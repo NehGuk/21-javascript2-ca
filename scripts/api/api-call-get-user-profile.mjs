0c7802724219d398e8d67e5ffd6cea915cf4ee3a
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-10 10:38:55 +0200

    Initial setup plus API call

--- scripts/api/api-call-get-user-profile.mjs
+++ scripts/api/api-call-get-user-profile.mjs
@@ -1,6 +1,29 @@
 export async function getUserProfile(url) {
     try {
 
+        const token = localStorage.getItem("accessToken");
+        const userName = localStorage.getItem("userName");
+
+        const fetchingOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        };
+
+        console.log("Workinggggg yeeeap");
+        const response = await fetch(url, fetchingOptions);
+        const userProfile = await response.json();
+        console.log(response);
+        console.log(userProfile);
+
+        async function displayUserProfile() {
+            console.log("Displaying");
+        }
+        displayUserProfile();
+
+
     } catch(error) {
         console.log(error);
     }
--- scripts/profile.mjs
+++ scripts/profile.mjs
@@ -1,1 +1,17 @@
 // After login, direct user to her respective profile page
+
+/// API URL 
+import { userProfileURL } from "./api/api-urls.mjs";
+import { extraProfileURL } from "./api/api-urls.mjs";
+const userName = localStorage.getItem("userName");
+
+const userURLToLoad = `${userProfileURL}${userName}${extraProfileURL}`;
+console.log(userURLToLoad);
+
+
+
+
+// Function to load user profile info
+import { getUserProfile } from "./api/api-call-get-user-profile.mjs";
+getUserProfile(userURLToLoad);
+
