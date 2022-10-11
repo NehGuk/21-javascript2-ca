bff0f27c3113e2dbdab0e92c1bae39dc1dd4c225
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-11 16:37:09 +0200

    Seach functionality implemented

--- scripts/api/api-call-get-posts.mjs
+++ scripts/api/api-call-get-posts.mjs
@@ -13,62 +13,66 @@ export async function getPosts(url) {
         console.log(response);
         console.log(posts);
         
-        function displayAllPosts() {
-            
-            const postsContainer = document.querySelector(".posts-container");
-            postsContainer.innerHTML = "";
-
-            for (let i = 0; i < posts.length; i++) {
-
-                // If the post contains no media, use a default image
-                if (!posts[i].media) {
-                    posts[i].media = "/assets/image-sample.jpg";
-                };
-                if (!posts[i].author.avatar) {
-                    posts[i].author.avatar = "/assets/image-sample.jpg";
-                };
-
+        // Creating a variable containing the posts array from the API call
+        window.posts = posts;
         
-                // Formatting the dates
-                const formattedDate = new Date(posts[i].created).toGMTString();
-
-                // Populating the HTML container
-                postsContainer.innerHTML += `
-                <div class="col">
-                    <div class="card h-100">
-                        <a href="/post.html?id=${posts[i].id}"><img src="${posts[i].media}" class="card-img-top" alt="..."></a>
-                        <div class="card-body">
-                        <a href="/post.html?id=${posts[i].id}" style="text-decoration: none"><h5 class="card-title">${posts[i].title}</h5></a>
-                        <p class="card-text">${posts[i].body}</p>
-                        </div>
-                        <div class="card-footer">
-                        <img src="${posts[i].author.avatar}" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
-                        <small class="text-muted"><strong>${posts[i].author.name}</strong>&emsp;</small>
-                            <small class="text-muted">${formattedDate}</small>
-                        </div>
-                        <div class="card-footer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                        </svg>
-                        <small class="text-muted">${posts[i]._count.comments} &emsp;</small>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
-                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                            <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                        <small class="text-muted">${posts[i]._count.reactions}</small>  
-                        </div>
-                    </div>
-                </div>
-                `;
-            }
         
-        };
-        displayAllPosts();
+        displayAllPosts(posts);
 
     } catch(error) {
         console.log(error);
     }
 };
 
+export function displayAllPosts(posts) {
+            
+    const postsContainer = document.querySelector(".posts-container");
+    postsContainer.innerHTML = "";
+
+    for (let i = 0; i < posts.length; i++) {
+
+        // If the post contains no media, use a default image
+        if (!posts[i].media) {
+            posts[i].media = "/assets/image-sample.jpg";
+        };
+        if (!posts[i].author.avatar) {
+            posts[i].author.avatar = "/assets/image-sample.jpg";
+        };
+
+
+        // Formatting the dates
+        const formattedDate = new Date(posts[i].created).toGMTString();
+
+        // Populating the HTML container
+        postsContainer.innerHTML += `
+        <div class="col">
+            <div class="card h-100">
+                <a href="/post.html?id=${posts[i].id}"><img src="${posts[i].media}" class="card-img-top" alt="..."></a>
+                <div class="card-body">
+                <a href="/post.html?id=${posts[i].id}" style="text-decoration: none"><h5 class="card-title">${posts[i].title}</h5></a>
+                <p class="card-text">${posts[i].body}</p>
+                </div>
+                <div class="card-footer">
+                <img src="${posts[i].author.avatar}" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+                <small class="text-muted"><strong>${posts[i].author.name}</strong>&emsp;</small>
+                    <small class="text-muted">${formattedDate}</small>
+                </div>
+                <div class="card-footer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                </svg>
+                <small class="text-muted">${posts[i]._count.comments} &emsp;</small>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                <small class="text-muted">${posts[i]._count.reactions}</small>  
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+};
 // Example
 // getPosts(allPostsURL);
--- scripts/index-search-bar.mjs
+++ scripts/index-search-bar.mjs
@@ -1,15 +1,44 @@
+
 // Function to get the API data (all posts)
-import { getPosts } from "./api/api-call-get-posts.mjs";
 import { allPostsURL } from "./api/api-urls.mjs";
+import { getPosts } from "./api/api-call-get-posts.mjs";
+
+// Importing display posts function
+import { displayAllPosts } from "./api/api-call-get-posts.mjs";
+
 getPosts(allPostsURL);
 
+// Search bar variables
 const searchInput = document.querySelector("#search-input");
-const searchButton = document.querySelector("#search-button");
+const clearButton = document.querySelector("#clear-button");
 
-searchInput.addEventListener("input", (e) => {
-    let value = e.target.value;
+// Search event listener
+searchInput.addEventListener("input", (event) => {
+    let value = event.target.value;
     console.log(value);
+    //console.log("Testing:", window.posts);
+    //console.log(window.posts[0].author.name);
+    let filteredArray = [];
 
+    // Add a for loop checking the input from the search bar
+    for (let i = 0; i < window.posts.length; i++) {
+        let currentPost = window.posts[i];
+
+        if (currentPost.title.includes(value) || currentPost.body.includes(value) || currentPost.author.name.includes(value)) {
+            filteredArray.push(currentPost);
+        }
+
+    }
+    displayAllPosts(filteredArray);
+    console.log(filteredArray);
 
 
 });
+
+
+
+
+
+
+
+
