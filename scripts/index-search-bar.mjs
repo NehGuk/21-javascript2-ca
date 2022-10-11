8d5a02234aca231f0cc2e458cd382500cf0d92f1
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-11 22:08:58 +0200

    Index post feed: search functionality ready

--- scripts/index-search-bar.mjs
+++ scripts/index-search-bar.mjs
@@ -1,23 +1,18 @@
+// Using ``window.posts`` to get the array from the previous API call
+// console.log(window.posts);
+// console.log(window.posts[0].author.name);
+
 // Importing display posts function
 import { displayAllPosts } from "./index-display-all-posts.mjs";
 
-// Function to get the API data (all posts)
-import { allPostsURL } from "./api/api-urls.mjs";
-import { getPosts } from "./api/api-call-get-posts.mjs";
-
-// Calling the function to lead the posts
-getPosts(allPostsURL);
-
-// Search bar variables
+// Variables
 const searchInput = document.querySelector("#search-input");
 const clearButton = document.querySelector("#clear-button");
+const errorMessageContainer = document.querySelector("#error-message-container");
 
-// Search event listener
+// Search field event listener
 searchInput.addEventListener("input", (event) => {
     let value = event.target.value;
-    console.log(value);
-    //console.log("Testing:", window.posts);
-    //console.log(window.posts[0].author.name);
     let filteredArray = [];
 
     // Add a for loop checking the input from the search bar
@@ -26,26 +21,22 @@ searchInput.addEventListener("input", (event) => {
 
         if (currentPost.title.includes(value) || currentPost.body.includes(value) || currentPost.author.name.includes(value)) {
             filteredArray.push(currentPost);
-            const errorMessageContainer = document.querySelector("#error-message-container");
             errorMessageContainer.innerHTML = ``;
 
         } else if (filteredArray.length === 0) {
-            const errorMessageContainer = document.querySelector("#error-message-container");
             errorMessageContainer.innerHTML = `
                 <p class="text-center text-secondary">Oooops! No posts match the search terms.</p>
             `;
         }
-
     }
     displayAllPosts(filteredArray);
-    console.log(filteredArray);
-
 });
 
-
 // Clear button
-/* clearButton.addEventListener("click", clearSearchField);    
+clearButton.addEventListener("click", clearSearchField);    
 function clearSearchField() {
     searchInput.value = "";
-} */
+    displayAllPosts(window.posts);
+    errorMessageContainer.innerHTML = ``;
+};
 
