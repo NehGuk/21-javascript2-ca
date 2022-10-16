aea2bba6c1ab948cbef90af663750ffdb1bc5a34
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-16 19:57:14 +0200

    Code cleanup

--- scripts/post-page/post-comments/create-new-comment.mjs
+++ scripts/post-page/post-comments/create-new-comment.mjs
@@ -1,14 +1,12 @@
 import { sendCommentURL } from "../../api/api-urls.mjs";
 import { sendNewComment } from "./api-call-send-comment.mjs";
 
-// get right URL to send comment
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const postID = urlParams.get("id");
 
 const postCommentURL = `${sendCommentURL}${postID}/comment`;
 
-// create object from form inputs
 const form = document.querySelector("#add-post-form");
 form.addEventListener("submit", createAndSendComment);
 function createAndSendComment(event) {
