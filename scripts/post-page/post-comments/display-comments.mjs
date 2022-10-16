22cc0529a87b78ae12fdf94e1262aba5db8c1c09
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-10-16 18:20:41 +0200

    User comments correctly displayed

--- scripts/post-page/post-comments/display-comments.mjs
+++ scripts/post-page/post-comments/display-comments.mjs
@@ -1,4 +1,20 @@
 export function displayComments() {
-  const commentsArray = post.comments;
-  console.log(commentsArray);
+  const postCommentsArray = post.comments;
+  const userCommentsContainer = document.querySelector("#user-comments-area");
+  userCommentsContainer.innerHTML = ``;
+  for (let i = 0; i < 3; i++) {
+    userCommentsContainer.innerHTML += `
+    <div class="container">
+    <div class="row">
+      <div class="col-1">
+        <a href="#"><img src="/assets/image-sample.jpg" width="32" height="32" class="rounded-circle border border-primary" style="width: 32; height: 32; object-fit: cover; border-width: 1px !important;" alt="User avatar"></a>
+      </div>
+      <div class="col">
+        <p><strong>${postCommentsArray[i].owner} |</strong> ${postCommentsArray[i].created} <br> ${postCommentsArray[i].body}</p>
+      </div>  
+    </div>
+    <hr>  
+    </div>    
+    `;
+  }
 }
